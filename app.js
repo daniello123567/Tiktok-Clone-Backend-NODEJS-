85cda7a60c5ed47ab4a3aced0f1f79e087db2477
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-10-09 17:51:45 +0100

    update

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import ConnectDB from './utils/connectdb.js';
 dotenv.config();
+const port = process.env.PORT || 3000;
+console.log(port);
+
 ConnectDB(process.env.CONNECT_URI);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.get('/',(req,res)=>{
+  res.send('WELCOME TO DANIEL TIKTOK BACKEND')
+})
 app.use('/videos',Videos);
 mongoose.connection.on('open',()=>{
-  app.listen(3000);
+  app.listen(port);
   console.log('no connection error, port sucessfully created');
 });
